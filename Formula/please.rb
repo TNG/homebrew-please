301a3f69884e6f2a106302af4ee9d19f31e25f00
TNG Technology Consulting <info@tngtech.com>
2023-04-07 10:54:40 +0000

    Update Please package to refs/tags/v0.0.10

--- Formula/please.rb
+++ Formula/please.rb
@@ -1,9 +1,9 @@
 class Please < Formula
   desc "An AI helper script to create CLI commands"
   homepage "https://tng.github.io/please-cli/"
-  version "0.0.8"
-  url "https://github.com/TNG/please-cli/archive/refs/tags/refs/tags/v0.0.8.zip"
-  sha256 ""
+  version "0.0.10"
+  url "https://github.com/TNG/please-cli/archive/.zip"
+  sha256 "96c5063faf2c4983903e6051c966c9319c066af5318cbb81bb1346b4b2a1da9e"
 
   depends_on "jq"
 
